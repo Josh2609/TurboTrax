042d7b274d394694a69103c86f584fc56063ac04
Josh Corps <jcorps@gmx.com>
2017-05-04 18:41:09 +0100

    Added checkpoints, lap counters and related scripts

---
+++ Assets/Scripts/lapCounter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lapCounter : MonoBehaviour {
+
+    public trackLapTrigger first;
+    public TextMesh textMesh;
+
+    trackLapTrigger next;
+
+    int _lap;
+
+    // Use this for initialization
+    void Start()
+    {
+        _lap = 0;
+        SetNextTrigger(first);
+        UpdateText();
+    }
+
+    // update lap counter text
+    void UpdateText()
+    {
+        if (textMesh)
+        {
+            textMesh.text = string.Format("Lap {0}", _lap);
+        }
+    }
+
+    // when lap trigger is entered
+    public void OnLapTrigger(trackLapTrigger trigger)
+    {
+        if (trigger == next)
+        {
+            if (first == next)
+            {
+                _lap++;
+                UpdateText();
+            }
+            SetNextTrigger(next);
+        }
+    }
+
+    void SetNextTrigger(trackLapTrigger trigger)
+    {
+        next = trigger.next;
+        SendMessage("OnNextTrigger", next, SendMessageOptions.DontRequireReceiver);
+    }
+}
---
+++ Assets/Scripts/trackLapTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackLapTrigger : MonoBehaviour {
+
+    // next trigger in the lap
+    public trackLapTrigger next;
+
+    // when an object enters this trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        lapCounter carLapCounter = other.gameObject.GetComponent<lapCounter>();
+        if (carLapCounter)
+        {
+            Debug.Log("lap trigger " + gameObject.name);
+            carLapCounter.OnLapTrigger(this);
+        }
+    }
+}
